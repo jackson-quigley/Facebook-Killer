fbd0fa5f68db0ed834dfc98de663fbf3af05adf0
c-beck <mr.cjbeck@gmail.com>
2018-04-21 21:54:01 -0400

    Edit of Handler

--- facebook-killer.sh
+++ facebook-killer.sh
@@ -6,10 +6,12 @@ user=$(dialog --title "Facebook Killer" --inputbox "Enter the username you want 
 
 pass=$(dialog --title "Facebook Killer" --passwordbox "Enter the password that goes with the username you want to mess with" 20 80 3>&1 1>&2 2>&3 3>&1)
 
+num=$(dialog --title "Facebook Killer" --inputbox "Enter the number of times you would like the script to run. Larger numbers are better for affecting the algorithm but will take longer" 20 80 3>&1 1>&2 2>&3 3>&1)
+
 dialog --title "Facebook Killer" --infobox "The program is now messing with the algorithms of facebook. Please be patient..." 20 80
 
-casperjs killer2.js --user=$user --pass=$pass > tmp &
-dialog --tailbox tmp 20 80
+casperjs killer2.js --user=$user --pass=$pass > /dev/null
 
 dialog --title "Facebook Killer" --msgbox "The algorithm has been messed with. Thank you for your patience." 20 80
+
 clear
